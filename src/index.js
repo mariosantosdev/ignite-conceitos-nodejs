cb9882a6194f5b91b1082d107a93ccf42b9ebc03
Mario Santos <mariodev7@gmail.com>
2022-09-11 21:29:33 -0300

    get todos of users

--- src/index.js
+++ src/index.js
@@ -48,8 +48,10 @@ app.post("/users", (request, response) => {
   return response.json(user);
 });
 
-app.get('/todos', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+app.get("/todos", checksExistsUserAccount, (request, response) => {
+  const { user } = request;
+
+  return response.json(user.todos);
 });
 
 app.post('/todos', checksExistsUserAccount, (request, response) => {
